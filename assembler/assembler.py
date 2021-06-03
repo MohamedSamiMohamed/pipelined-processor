7da35ab81f12898995175ff710c1e524a55cf0a6
Areej99 <areejraafat99@gmail.com>
2021-06-04 00:38:48 +0200

    assembler done

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -7,6 +7,20 @@ with open("dectionary.txt") as dictFile:
             dict[str(key)]=val
 print(dict)
 
+
+def expandInstruction(instruction):
+    instructions=[]
+    op=instruction.split(" ")[1]
+    if(instruction.find("CALL")!=-1):
+        instructions.append(instruction)
+        instructions.append("DECSP")
+        instructions.append(" ".join(["JMP",op]))
+
+    elif(instruction.find("PUSH")!=-1):
+        instructions.append(instruction)
+        instructions.append("DECSP") 
+    return instructions
+
 def decodeInstruction(instruction):
     code=[""]
     instItems=instruction.split(" ")
@@ -47,7 +61,7 @@ def decodeInstruction(instruction):
     return code
 
 instructions=[]
-with open("code.txt") as codeFile:
+with open("our test case .txt") as codeFile:
     for line in codeFile:
         line = line.upper()
         line = line.replace('\t', '')
@@ -55,12 +69,17 @@ with open("code.txt") as codeFile:
         if(len(line)!=0):
             if(line[0]!='#' and line[0]!='\n'):
                 line=re.split('#',line)[0]
-                line=line.rstrip()               
-                instructions.append(line)
+                line=line.rstrip()
+                if(line.find("CALL")!=-1 or line.find("PUSH")!=-1):
+                    expandedInstruction=expandInstruction(line)
+                    for inst in expandedInstruction:
+                        instructions.append(inst)
+                else:               
+                    instructions.append(line)
 
 print(instructions)
 memlocation=0
-decodedInstruction=["0000000000000000"]*32
+decodedInstruction=["0000000000000000"]*50
 for instruction in instructions:
     if(instruction.find(".ORG")!=-1):
         print("i'm org")
@@ -78,7 +97,8 @@ for instruction in instructions:
             memlocation+=1
 
 
-
+fOut = open("assemblerOutput.txt","w")
+fOut.write("\n".join(decodedInstruction))
 memory="// memory data file (do not edit the following line - required for mem load use) \n// instance=/fetchstage/Mem/RamArray \n// format=mti addressradix=d dataradix=s version=1.0 wordsperline=1\n"
 for instruction in instructions:
     memory+=str(memlocation)+": "
---
+++ test.py
@@ -0,0 +1,19 @@
+f= open("outputs.txt","a")
+# i =0
+# f.write("0000000000000010\n")
+# f.write("0000000000000000\n")
+# while (i<32):
+#     if i == 20 :
+#         f.write("{0:5b}".format(i).replace(" ","0"))
+#         f.write("\n")
+#         f.write("{0:5b}".format(31).replace(" ","0"))
+#     elif i== 29 :
+#         f.write("{0:5b}".format(i).replace(" ","0"))
+#         f.write("\n")
+#         f.write("{0:5b}".format(31).replace(" ","0"))
+#         f.write("\n")
+#         f.write("{0:5b}".format(28).replace(" ","0"))
+#     else:
+#         f.write("{0:5b}".format(i).replace(" ","0"))
+#     f.write("\n")
+#     i+=1
