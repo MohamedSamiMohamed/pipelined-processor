b370f2e112ec7102df16d1fa6d32f6d66701bf0c
MohamedSamiMoahmed <mohamed.samy9956@gmail.com>
2021-06-05 17:17:48 +0200

    EX-MEM and ex-stage are ready

--- ex-mem-buffer.vhd
+++ ex-mem-buffer.vhd
@@ -6,16 +6,16 @@ ENTITY ExMemBuffer IS PORT (
     result, readData2, increamentedPC : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
     Rdst : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
     mem_cs : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
-    wb_cs : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-    q : OUT STD_LOGIC_VECTOR(109 DOWNTO 0)
+    wb_cs : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    q : OUT STD_LOGIC_VECTOR(108 DOWNTO 0)
 );
 END ENTITY;
 ARCHITECTURE ExMem OF ExMemBuffer IS
 BEGIN
-    q(3 DOWNTO 0) <= wb_cs;
-    q(10 DOWNTO 4) <= mem_cs;
-    q(13 DOWNTO 11) <= Rdst;
-    q(45 DOWNTO 14) <= increamentedPC;
-    q(77 DOWNTO 46) <= readData2;
-    q(109 DOWNTO 78) <= result;
+    q(2 DOWNTO 0) <= wb_cs;
+    q(9 DOWNTO 3) <= mem_cs;
+    q(12 DOWNTO 10) <= Rdst;
+    q(44 DOWNTO 13) <= increamentedPC;
+    q(76 DOWNTO 45) <= readData2;
+    q(108 DOWNTO 77) <= result;
 END ExMem;
